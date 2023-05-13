f8ba7a5340738992b38c6183795db46b3348e84a
Shivaprasad <51282721+shivarao101@users.noreply.github.com>
2023-05-13 21:21:15 +0530

    Add files via upload

---
+++ contact.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import sqlite3
+cur=sqlite3.connect('contact.db',check_same_thread=False)
+st.write('Contact Information')
+with st.form(key='Contact'):
+    name=st.text_input('Name:')
+    email=st.text_input('Email-id:')
+    address=st.text_area('Address:')
+    bt=st.form_submit_button('Submit')
+    if bt:
+        cur.execute('''CREATE TABLE IF NOT EXISTS 
+                    details (Name TEXT(50), Email TEXT(50), Address TEXT(100))
+                    ''')
+        cur.commit()
+        cur.execute('''INSERT INTO details(Name, Email, Address) 
+                    VALUES(?,?,?)
+                    ''',(name,email,address))
+        cur.commit()
+        st.success('Form submitted')
+        
